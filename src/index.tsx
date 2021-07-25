39f56badd260a335b520ab06b4dc380aaa0ac9c5
mahesh <chambharem7@gmail.com>
2021-07-25 21:07:09 +0530

    publishing

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
 
+
+ ReactGA.initialize('G-ZCW2RVT17P');
+ ReactGA.pageview(window.location.pathname + window.location.search);
 
 
 ReactDOM.render(
   <React.StrictMode>
+   
     <App />
   </React.StrictMode>,
   document.getElementById('root')
